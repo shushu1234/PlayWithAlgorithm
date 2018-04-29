7a4519628f9d0f78229c2515cc661460abd8b94f
shushu1234 <1395752337@qq.com>
2018-04-29 21:13:22 +0800

    add union

---
+++ src/com/liuyao/union/UnionFind2.java
@@ -0,0 +1,52 @@
+package com.liuyao.union;
+
+/**
+ * Created By liuyao on 2018/4/29 17:43.
+ */
+public class UnionFind2 {
+    private int[] parent; //parentb[i]表示第一个元素所指向的父节点
+    private int count; //数据个数
+
+    public UnionFind2(int count) {
+        parent = new int[count];
+        // 初始化, 每一个parent[i]指向自己, 表示每一个元素自己自成一个集合
+        for (int i = 0; i < count; i++) {
+            parent[i] = i;
+        }
+    }
+
+    // 查找过程, 查找元素p所对应的集合编号
+    // O(h)复杂度, h为树的高度
+    private int find(int p) {
+        assert (p >= 0 && p < count);
+        // 不断去查询自己的父亲节点, 直到到达根节点
+        // 根节点的特点: parent[p] == p
+        while (p != parent[p]) {
+            p = parent[p];
+        }
+        return p;
+    }
+
+    // 查看元素p和元素q是否所属一个集合
+    // O(h)复杂度, h为树的高度
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    // 合并元素p和元素q所属的集合
+    // O(h)复杂度, h为树的高度
+    public void unionElements(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+        if (pRoot == qRoot) {
+            return;
+        }
+        parent[pRoot] = qRoot;
+    }
+
+    public static void main(String[] args) {
+        int n = 100000;
+
+        UnionFindTestHelper.testUF2(n);
+    }
+}
